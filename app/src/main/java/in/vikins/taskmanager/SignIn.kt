7265b0fe2ba46171ea110c9d31f928828a82705f
Shubhasai <mshubhasai2002@gmail.com>
2021-09-22 23:56:10 +0530

    Ui Improved, Search Implemented,Cloud data storage, Sawo passwordless auth

--- app/src/main/java/in/vikins/taskmanager/SignIn.kt
+++ app/src/main/java/in/vikins/taskmanager/SignIn.kt
@@ -15,7 +15,16 @@ class SignIn : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
         Handler().postDelayed({
-            startActivity(Intent(this,SAuthentication::class.java))
+            startActivity(Intent(this,MainActivity::class.java))
+            Sawo(
+                this,
+                "0651d4d9-f3f3-49a3-b7de-6eca34704af8", // your api key
+                "614b574309e2ab2081566855dla27xXeLSosIUUdK3b1k9AE"  // your api key secret
+
+            ).login(
+                "email", // can be one of 'email' or 'phone_number_sms'
+                MainActivity::class.java.name // Callback class name
+            )
             finish()
         }, 3000)
     }
